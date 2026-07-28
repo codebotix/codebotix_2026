f52e98af77154d98ff38cfb9c0763f05becebfb2
Saif Ali Khan <23uca004@loyolacollege.edu>
2026-07-29 01:46:13 +0530

    code2

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&family=Inter:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        {/* JSON-LD Structured Data */}
+        {/* JSON-LD Structured Dathi */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
